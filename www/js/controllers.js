ec13859d605c4463c9312de8c62efa106ec92240
Unknown <monteiro1breno@gmail.com>
2017-03-06 22:23:28 -0300

    Adding donation Modal

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,10 +7,10 @@ function ($scope, $stateParams) {
 
 }])
    
-.controller('doaOMaterialCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('doaOMaterialCtrl', ['$scope', '$stateParams', '$ionicModal', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $ionicModal) {
 
     $scope.abrigos = [
                 {
@@ -91,8 +91,39 @@ function ($scope, $stateParams) {
                 $scope.instituicaoSelecionadaObject = valor
             }
         };
+        $ionicModal.fromTemplateUrl('templates/doacaoModal.html',{
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+        });
+
+        $scope.openModal = function(){
+            $scope.modal.show();
+        };
+
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+        
+    //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
+        
+    // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+        // Execute action
+        });
+        
+    // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+        // Execute action
+        });
+
         $scope.realizarDoacao = function(item){
-            console.log(item);
+            $scope.itemSelecionado = item;
+            $scope.openModal();
         }
         
 }])
